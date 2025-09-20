f3a1b94e6f133d51df79390b5e347fdf37bfa14e
khayfire <kayode354@gmail.com>
2025-09-20 11:59:32 +0100

    react

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,46 +1,27 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
-import UserModal from "@/components/common/UserModal";
-import { UserProps, UserData } from "@/interfaces";
-import { useState } from "react";
+import { UserProps } from "@/interfaces";
 
 interface UsersPageProps {
-  posts: UserProps[];
+  users: UserProps[];
 }
 
-const Users: React.FC<UsersPageProps> = ({ posts }) => {
-  const [allUsers, setAllUsers] = useState(posts);
-  const [showModal, setShowModal] = useState(false);
-
-  const handleAddUser = (newUser: UserData) => {
-    setAllUsers([{ ...newUser, id: allUsers.length + 1 }, ...allUsers]);
-  };
-
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <div className="flex justify-between mb-6">
-          <h1 className="text-2xl font-semibold">Users</h1>
-          <button
-            className="bg-green-700 px-4 py-2 rounded-full text-white"
-            onClick={() => setShowModal(true)}
-          >
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Users List</h1>
+          <button className="bg-blue-700 px-4 py-2 rounded-full text-white">
             Add User
           </button>
         </div>
-        <div className="grid grid-cols-3 gap-4">
-          {allUsers?.map((user: UserProps) => (
-            <UserCard key={user.id} {...user} />
+        <div className="grid grid-cols-3 gap-2">
+          {users?.map((user: UserProps, key: number) => (
+            <UserCard key={key} {...user} />
           ))}
         </div>
-
-        {showModal && (
-          <UserModal
-            onClose={() => setShowModal(false)}
-            onSubmit={handleAddUser}
-          />
-        )}
       </main>
     </div>
   );
@@ -48,11 +29,11 @@ const Users: React.FC<UsersPageProps> = ({ posts }) => {
 
 export async function getStaticProps() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const posts = await response.json();
+  const users = await response.json();
 
   return {
     props: {
-      posts,
+      users,
     },
   };
 }
